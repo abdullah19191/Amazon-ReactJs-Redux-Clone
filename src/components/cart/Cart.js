bd720ab64bc43c1b4016f4f6feb528d51bc0219e
Abdullah Bin Rashid <71758955+abdullah19191@users.noreply.github.com>
2023-03-05 14:08:21 +0500

    Add to Cart half page made responsive

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,43 +1,60 @@
-import { Divider } from '@mui/material'
-import React from 'react'
-import Option from './Option'
+import { Divider } from "@mui/material";
+import React from "react";
+import Option from "./Option";
 
 const cart = () => {
   return (
-    <div className='w-full h-auto  md:min-h-[100vh] relative top-16'>
-        <div className="flex mx-auto my-0 px-5 py-10 justify-around">
-              <div className="bg-white flex-[.7] px-5 py-3 border-1 rounded-md">
-                <h1 className='font-bold text-3xl'>Shopping Cart</h1>
-                <h5 className='font-semibod text-1xl text-blue-700 my-3'>Select All items</h5>
-                <Divider/>
-                <div className="flex py-4 pr-[5px] pl-[20px]">
-                    <div className="">
-                        <img src="https://rukminim1.flixcart.com/image/300/300/kll7bm80/smartwatch/c/1/n/43-mo-sw-sense-500-android-ios-molife-original-imagyzyycnpujyjh.jpeg?q=70" alt="" 
-                        className='w-[80%]'
-                        />
-                    </div>
-                    <div className="">
-                        <h3 className='font-[600] text-lg'>Molife Sense 500 Smartwatch  (Black Strap, Freesize)<span className=' ml-4 font-extrabold text-xl' >$786.88</span></h3>
-                       <h3 className='font-[600] text-lg'>Smart Watches</h3>
-                     <p className='mt-2 mb-2 text-[#c45500] text-md font-[500]'>Usually dispatched in 8 days</p>
-                     <p className=' mb-4 text-md font-[500]'>Eligible for free Shipping</p>
-                     <img src="https://m.media-amazon.com/images/G/31/marketing/fba/fba-badge_18px-2x._CB485942108_.png" alt="logo" className=' cursor-pointer w-[20%] mb-3 '/>
-                     <div className="">
-                        <Option/>
-                     </div>
-                    </div>
-                </div>
-                     <Divider/>
-                     <div className=" flex justify-end p-2">
-                        <h3 className='font-semibold text-2xl '>Subtotal(1 items):<span className='font-[700]'>$786.88</span></h3>
-                     </div>
+    <div className="w-full h-auto  md:h-screen relative top-16">
+      <div className="flex flex-col md:flex-row mx-auto my-0 px-5 py-10 justify-around items-start w-[95%]">
+        <div className="bg-white flex-[.7] px-5 py-3 border-1 rounded-md">
+          <h1 className="font-bold text-3xl">Shopping Cart</h1>
+          <h5 className="font-semibod text-1xl text-blue-700 my-3">
+            Select All items
+          </h5>
+          <Divider />
+          <div className="md:flex py-4 pr-[5px] pl-[20px] items-center justify-center">
+            <div className="  justify-center md:justify-start flex md:flex-none">
+              <img
+                src="https://rukminim1.flixcart.com/image/300/300/kll7bm80/smartwatch/c/1/n/43-mo-sw-sense-500-android-ios-molife-original-imagyzyycnpujyjh.jpeg?q=70"
+                alt=""
+                className="md:w-[80%] w-36"
+              />
+            </div>
+            <div className="">
+              <h3 className="font-[600] text-lg">
+                Molife Sense 500 Smartwatch (Black Strap, Freesize)
+                <span className="  font-[750] text-xl">$786.88</span>
+              </h3>
+              <h3 className="font-[600] text-lg">Smart Watches</h3>
+              <p className="mt-2 mb-2 text-[#c45500] text-md font-[500]">
+                Usually dispatched in 8 days
+              </p>
+              <p className=" mb-4 text-md font-[500]">
+                Eligible for free Shipping
+              </p>
+              <img
+                src="https://m.media-amazon.com/images/G/31/marketing/fba/fba-badge_18px-2x._CB485942108_.png"
+                alt="logo"
+                className=" cursor-pointer w-[20%] mb-3 "
+              />
+              <div className="">
+                <Option />
               </div>
-              <div className="bg-white flex-[.2] border-1 rounded-md mr-[-10px] py-0 px-12">
-                <h1>right</h1>
-              </div>
+            </div>
+          </div>
+          <Divider />
+          <div className=" flex justify-end p-2">
+            <h3 className="font-semibold text-2xl ">
+              Subtotal(1 items):<span className="font-[700]">$786.88</span>
+            </h3>
+          </div>
         </div>
+        <div className="bg-white flex-[.2] border-1 rounded-md mr-[-10px] py-0 px-12">
+          <h1>right</h1>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default cart
+export default cart;
--- src/components/cart/Option.js
+++ src/components/cart/Option.js
@@ -3,7 +3,8 @@ import Rating from "material-ui-rating";
 
 const Option = () => {
   return (
-    <div className='flex space-x-5 items-center '>
+    <div className="">
+    <div className='flex md:space-x-5 space-x-2 items-center '>
         <select name="" id="" className='border-2 rounded-2xl bg-slate-100 p-2 pr-4'>
             <option value="1">1</option>
             <option value="2">2</option>
@@ -13,7 +14,11 @@ const Option = () => {
         <p className='text-blue-400 font-normal'>Delete</p><span>|</span>
         <p className='text-blue-400 font-normal'>Save Or Later</p><span>|</span>
         <p className='text-blue-400 font-normal'>See More like this</p>
+    </div>
+        <div className="">
         <Rating value={4} max={5} name="size-large"  onChange={(value) => console.log(`Rated with value ${value}`)}/>
+        </div>
+
     </div>
   )
 }
