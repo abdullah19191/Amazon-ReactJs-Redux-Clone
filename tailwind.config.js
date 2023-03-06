c158535da214e0d4b96569ff957ba2da8107ec28
Abdullah Bin Rashid <71758955+abdullah19191@users.noreply.github.com>
2023-03-07 00:13:53 +0500

    Configuration added for tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth:{
+        container:"1440px"
+      },
+      screens: {
+        xs:"320px",
+        sm:"375px",
+        sml:"500px",
+        md:"667px",
+        mdl:"768px",
+        lg:"960px",
+        lgl:"1024px",
+        xl:"1280px"
+      }
+    },
   },
   plugins: [],
 }
