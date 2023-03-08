2bf45d072e0d732a95d16a0b253fb5348d6524e0
Abdullah Bin Rashid <71758955+abdullah19191@users.noreply.github.com>
2023-03-08 15:52:51 +0500

    Sign in Formatted

--- src/components/Authenticate/Signin.js
+++ src/components/Authenticate/Signin.js
@@ -2,25 +2,24 @@ import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const Signin = () => {
-  const [logdata,setData] = useState({
-      email:"",
-      password:""
+  const [logdata, setData] = useState({
+    email: "",
+    password: "",
   });
-  
- const addData = (e)=>{
-    const {name,value} = e.target;
 
-      setData(()=>{
-        return {
-            ...logdata,
-            [name]:value
-        }
-      })
-    console.log(e.target.value)
-  }
+  const addData = (e) => {
+    const { name, value } = e.target;
 
-    return (
+    setData(() => {
+      return {
+        ...logdata,
+        [name]: value,
+      };
+    });
+    console.log(e.target.value);
+  };
 
+  return (
     <section className=" w-[100%] relative top-16 min-h-screen bg-white">
       <div className="">
         <div className="">
@@ -37,7 +36,7 @@ const Signin = () => {
             </h1>
             <div className="flex flex-col lg:w-11/12 w-[110%]">
               <label htmlFor="email" className="mb-1 font-medium text-lg">
-              Email
+                Email
               </label>
               <input
                 type="text"
@@ -52,18 +51,26 @@ const Signin = () => {
               <label htmlFor="password" className="mb-1 font-medium text-md">
                 Password
               </label>
-              <input type="password" name="password" onChange={addData} value={logdata.password} placeholder="At least 6 char" class="relative mb-2  p-2  border-2  outline-none rounded-md"></input>
+              <input
+                type="password"
+                name="password"
+                onChange={addData}
+                value={logdata.password}
+                placeholder="At least 6 char"
+                class="relative mb-2  p-2  border-2  outline-none rounded-md"
+              ></input>
             </div>
-            <button className=" inline-block font-medium bg-gradient-to-r from-[#f7dfa5] to-[#f0c14b]  p-2 rounded-sm mt-2 cursor-pointer border-2 border-[#9c7e31] border-solid mb-7 h-10 w-full   text-[#111] bg-[#f3d078] outline-none ">Continue</button>
+            <button className=" inline-block font-medium bg-gradient-to-r from-[#f7dfa5] to-[#f0c14b]  p-2 rounded-sm mt-2 cursor-pointer border-2 border-[#9c7e31] border-solid mb-7 h-10 w-full   text-[#111] bg-[#f3d078] outline-none ">
+              Continue
+            </button>
           </form>
-        <p className="mt-2 mb-2 pt-1 text-md font-semibold space-x-3">
+          <p className="mt-2 mb-2 pt-1 text-md font-semibold space-x-3">
             New To Amazon?
-              </p>
-              <NavLink
-            to="/register">
-              <button className="text-lg w-[300px] transition  bg-gradient-to-b h=[29px] hover:bg-slate-400 border-gray-700 border-2 border-solid rounded-[3.375rem] shadow from-[#f7f8fa] p-2 to-[#e7e9ec]">
-                   Register
-              </button>
+          </p>
+          <NavLink to="/register">
+            <button className="text-lg w-[300px] transition  bg-gradient-to-b h=[29px] hover:bg-slate-400 border-gray-700 border-2 border-solid rounded-[3.375rem] shadow from-[#f7f8fa] p-2 to-[#e7e9ec]">
+              Register
+            </button>
           </NavLink>
         </div>
       </div>
