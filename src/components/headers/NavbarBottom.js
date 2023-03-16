38cd3ac4cef997da7ae6740bdc90f4676747250b
Abdullah Bin Rashid <71758955+abdullah19191@users.noreply.github.com>
2023-03-16 20:04:23 +0500

    SideNavbar completed

--- src/components/headers/NavbarBottom.js
+++ src/components/headers/NavbarBottom.js
@@ -2,19 +2,19 @@ import React, { useEffect, useRef, useState } from "react";
 import MenuIcon from "@mui/icons-material/Menu";
 import SidebarNav from "./SidebarNav";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import CloseIcon from '@mui/icons-material/Close';
-import {motion} from "framer-motion"
+import CloseIcon from "@mui/icons-material/Close";
+import { motion } from "framer-motion";
 
 const NavbarBottom = () => {
-    const ref = useRef();
+  const ref = useRef();
   const [sidebar, setSidebar] = useState(false);
-  useEffect(()=>{
-    document.body.addEventListener("click",(e)=>{
-        if(e.target.contains(ref.current)){
-            setSidebar(false)
-        }
-    })
-  })
+  useEffect(() => {
+    document.body.addEventListener("click", (e) => {
+      if (e.target.contains(ref.current)) {
+        setSidebar(false);
+      }
+    });
+  });
   return (
     <div className="w-full bg-[#131139] h-9 text-white px-4 flex items-center">
       <ul className="flex items-center tracking-wide text-sm gap-4">
@@ -30,24 +30,35 @@ const NavbarBottom = () => {
         <li className="headerHover">Gift Cards</li>
         <li className="headerHover">Sell</li>
         <li className="headerHover">Register</li>
-      </ul> 
+      </ul>
       {sidebar && (
         <div className=" w-full h-screen text-black fixed top-0 left-0 bg-[#131921] bg-opacity-50 z-50">
           <div className=" w-full h-full relative">
-          <motion.div ref={ref} initial={{x:-500,opacity:0}} animate={{x:0,opacity:1}} transition={{duration:.5}} className=" bg-white w-[350px] h-full border border-black">
-          <div className=" w-full bg-[#131921] text-white py-4 px-6 flex items-center gap-4 text-lg ">
+            <motion.div
+              ref={ref}
+              initial={{ x: -500, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 0.5 }}
+              className=" bg-white w-[350px] h-full border border-black"
+            >
+              <div className=" w-full bg-[#131921] text-white py-4 px-6 flex items-center gap-4 text-lg ">
                 <AccountCircleIcon />
                 <h1 className=" font-bold text-md tracking-wide">
                   Hellow, Sign In
                 </h1>
               </div>
-            <SidebarNav/>
-            <SidebarNav/>
-            <SidebarNav/>
-            <SidebarNav/>
-            <span onClick={()=>setSidebar(false)} className=" top-0 left-[356px] w-10 h-10 absolute text-black flex items-center justify-center
+              <SidebarNav />
+              <SidebarNav />
+              <SidebarNav />
+              <SidebarNav />
+              <span
+                onClick={() => setSidebar(false)}
+                className=" top-0 left-[356px] w-10 h-10 absolute text-black flex items-center justify-center
              bg-gray-200 hover:bg-red-600  hover:text-white border duration-300
-            "><CloseIcon/></span>
+            "
+              >
+                <CloseIcon />
+              </span>
             </motion.div>
           </div>
         </div>
--- src/components/headers/SidebarNav.js
+++ src/components/headers/SidebarNav.js
@@ -1,21 +1,36 @@
-import React from 'react'
-import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import React from "react";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 
 const SidebarNav = () => {
   return (
     <div>
-              <div className=" py-3 border-b-[1px] border-b-gray-300 ">
-                <h3 className=" text-lg  font-bold mb-1 px-6">
-                  Digital Content & Devices
-                </h3>
-                <ul className='text-md'>
-                    <li className="flex items-center justify-between hover:bg-zinc-200 px-6 py-2">Amazon Music<span><KeyboardArrowRightIcon/></span></li>
-                    <li className="flex items-center justify-between hover:bg-zinc-200 px-6 py-2">Amazon Music<span><KeyboardArrowRightIcon/></span></li>
-                    <li className="flex items-center justify-between hover:bg-zinc-200 px-6 py-2">Amazon Music<span><KeyboardArrowRightIcon/></span></li>
-                </ul>
-              </div>
-            </div>
-  )
-}
+      <div className=" py-3 border-b-[1px] border-b-gray-300 ">
+        <h3 className=" text-lg  font-bold mb-1 px-6">
+          Digital Content & Devices
+        </h3>
+        <ul className="text-md">
+          <li className="flex items-center justify-between hover:bg-zinc-200 px-6 py-2">
+            Amazon Music
+            <span>
+              <KeyboardArrowRightIcon />
+            </span>
+          </li>
+          <li className="flex items-center justify-between hover:bg-zinc-200 px-6 py-2">
+            Amazon Music
+            <span>
+              <KeyboardArrowRightIcon />
+            </span>
+          </li>
+          <li className="flex items-center justify-between hover:bg-zinc-200 px-6 py-2">
+            Amazon Music
+            <span>
+              <KeyboardArrowRightIcon />
+            </span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-export default SidebarNav
+export default SidebarNav;
