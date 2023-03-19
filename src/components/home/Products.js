12806dde9430a2c3ae4e57a1324b3f8820371e49
Abdullah Bin Rashid <71758955+abdullah19191@users.noreply.github.com>
2023-03-19 23:19:42 +0500

    Products Ui design in process

--- src/components/home/Products.js
+++ src/components/home/Products.js
@@ -12,18 +12,20 @@ const Products = () => {
     </div>
   }
   return (
-    <div className="w-full  grid grid-cols-4 gap-10 mx-auto px-4">
+    <div className="w-full  grid grid-cols-2 gap-10 mx-auto px-4 mt-12 md:grid-cols-4">
       {state.product.data &&
         state.product.data.map((e) => {
           return (
-            <div className="bg-white border-1 h-auto hover:border-transparent hover:shadow-xl relative duration-200  border-gray-200 py-6 z-30 shadow flex flex-col">
-              <div className="">
+            <div className="bg-white border-1  hover:border-transparent hover:shadow-xl relative duration-200 gap-4  border-gray-200 py-6 z-30 shadow flex flex-col">
+              <div className="h-auto flex justify-center items-center w-full">
               <img src={e.image} alt="" className="w-52  h-64 object-contain" />
               </div>
-              <div className="">
-              <h1>{e.title}</h1>
-              <h1>{e.description}$</h1>
+              <div className=" px-4">
+              <div className=" flex gap-12 justify-between items-center">
+              <h1 className=" font-medium tracking-wide text-lg text-blue-900">{e.title.substring(0,20)}</h1>
+              {/* <h1>{e.description}$</h1> */}
               <h1>{e.price}$</h1>
+              </div>
               </div>
             </div>
           );
