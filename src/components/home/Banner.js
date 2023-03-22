447fd5614c44634bcbc9c55cc9f4350a1eef5df4
Abdullah Bin Rashid <71758955+abdullah19191@users.noreply.github.com>
2023-03-22 22:20:20 +0500

    ft

--- src/components/home/Banner.js
+++ src/components/home/Banner.js
@@ -1,35 +1,55 @@
 import React from "react";
 import Carousel from "react-material-ui-carousel";
 
-
 function Banner() {
   return (
     <div className="w-full h-auto -z-50">
-    <Carousel
-      autoPlay={true}
-      animation="slide"
-      indicators={false}
-      navButtonsWrapperProps={{   // Move the buttons to the bottom. Unsetting top here to override default style.
-        style: {
-            bottom: '0',
-            top: 'unset'
-        }
-    }} 
-      navButtonsAlwaysVisible={false}
-      cycleNavigation={true}
-      navButtonsProps={{
-        style: {
-          borderRadius: 0,
-          marginBottom:4,
-        },
-      }}
-    >
-        <img src= './assets/bannerImgFive.jpg' alt="" className=" object-cover object-center" />
-        <img src= './assets/bannerImgFour.jpg' alt="" className="object-cover object-center" />
-        <img src= './assets/bannerImgThree.jpg' alt="" className=" object-cover object-center" />
-        <img src= './assets/bannerImgTwo.jpg' alt="" className="object-cover object-center" />
-        <img src= './assets/bannerImgOne.jpg' alt="" className="object-cover object-center" />
-    </Carousel>
+      <Carousel
+        autoPlay={true}
+        animation="slide"
+        indicators={false}
+        navButtonsWrapperProps={{
+          // Move the buttons to the bottom. Unsetting top here to override default style.
+          style: {
+            bottom: "0",
+            top: "unset",
+          },
+        }}
+        navButtonsAlwaysVisible={false}
+        cycleNavigation={true}
+        navButtonsProps={{
+          style: {
+            borderRadius: 0,
+            marginBottom: 4,
+          },
+        }}
+      >
+        <img
+          src="./assets/bannerImgFive.jpg"
+          alt=""
+          className=" object-cover object-center"
+        />
+        <img
+          src="./assets/bannerImgFour.jpg"
+          alt=""
+          className="object-cover object-center"
+        />
+        <img
+          src="./assets/bannerImgThree.jpg"
+          alt=""
+          className=" object-cover object-center"
+        />
+        <img
+          src="./assets/bannerImgTwo.jpg"
+          alt=""
+          className="object-cover object-center"
+        />
+        <img
+          src="./assets/bannerImgOne.jpg"
+          alt=""
+          className="object-cover object-center"
+        />
+      </Carousel>
     </div>
   );
 }
