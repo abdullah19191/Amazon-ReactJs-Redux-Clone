c183b4add6f942251bc2497c46d4c8000264e218
Abdullah Bin Rashid <71758955+abdullah19191@users.noreply.github.com>
2023-03-24 23:51:14 +0500

    quantity inc and dic added

--- src/redux/slice/cartSlice.js
+++ src/redux/slice/cartSlice.js
@@ -9,6 +9,18 @@ export const cartSlice = createSlice({
     name:'cart',
     initialState,
     reducers:{
+        incrementQuantity:(state,action)=>{
+             const items = state.products.find((item)=>item.id === action.payload);
+             items.quantity++;
+        },
+        decrementQuantity:(state,action)=>{
+            const items = state.products.find((item)=>item.id === action.payload.id);
+            if(items.quantity === 1){
+                items.quantity = 1;
+            } else{
+                items.quantity--;
+            }
+        },
         addToCart:(state,action)=>{
             const item = state.products.find((item)=>item.id === action.payload.id)
             if(item){
