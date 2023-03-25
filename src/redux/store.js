25f9bbbad129af78f1beffedc356d34dcaebe9e1
Abdullah Bin Rashid <71758955+abdullah19191@users.noreply.github.com>
2023-03-25 12:55:30 +0500

    local storage added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,25 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import productReducer from "./slice/productslice";
-import cartReducer from "./slice/cartSlice"
-export const store = configureStore({
-  reducer: {
-    product: productReducer,
-    cart: cartReducer,
-  },
+import cartReducer from "./slice/cartSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+import thunk from "redux-thunk";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+const mainReducer = combineReducers({
+  product: productReducer,
+  cart: cartReducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, mainReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== "production",
+  middleware: [thunk],
+});
+
+export const persistor = persistStore(store);
